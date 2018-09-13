8cadf1826b1fc87fe496dd0c5eda75008ed3046b
root <utsav.krishnan.cse15@iitbhu.ac.in>
2018-09-13 21:54:32 +0530

    Solved window close, if tabsCount==1.

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
 chrome.webNavigation.onCompleted.addListener(function() {
-  chrome.tabs.getSelected(function(tab) {
-    chrome.tabs.remove(tab.id, function() { });
-  });
+  chrome.tabs.query({}, function(foundTabs) {
+        var tabsCount = foundTabs.length;
+        console.info("tabsCount = " + tabsCount);
+        if(tabsCount == 1){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              var tab = tabs[0];
+              chrome.tabs.update(tab.id, {url: "https://www.google.com/"});
+          });
+        }else{
+          chrome.tabs.getSelected(function(tab) {
+            chrome.tabs.remove(tab.id, function() { });
+          });
+        }
+    });
 }, {url: [{urlMatches : 'http://192.168.249.1:1000/keepalive*'}]});
 
 chrome.runtime.onInstalled.addListener(function (object) {
